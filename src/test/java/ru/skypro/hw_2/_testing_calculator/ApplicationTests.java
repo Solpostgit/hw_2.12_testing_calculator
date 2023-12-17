84f43396cc8d713c464956b792c6e1df986d69bc
Solpostgit <begemot_off@mail.ru>
2023-12-17 15:27:55 +0300

    Initial commit

---
+++ src/test/java/ru/skypro/hw_2/_testing_calculator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.hw_2._testing_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
